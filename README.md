9cf35e2ceb43b79298af4ab267d8d66293e8c284
Evgeny Bukreev <ebukreev@ptsecurity.com>
2024-02-25 10:34:05 +0300

    add shadow jar

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # type-systems-course
+
+Сборка проекта
+
+```bash
+./gradlew build
+```
+
+Запуск
+
+```bash
+java -jar build/libs/typechecker.jar
+```
+
+Запуск тестов
+
+```bash
+./gradlew test
+```
+
+Программа при запуске ожидает программу на языке
+Stella на stdin с EOF (Ctrl+D) в конце, после чего производит
+проверку типов.
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     kotlin("jvm") version "1.9.0"
     application
+    java
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "dev.bukreev"
@@ -24,5 +28,17 @@ kotlin {
 }
 
 application {
-    mainClass.set("MainKt")
+    mainClass.set("dev.bukreev.types.MainKt")
+}
+tasks {
+    named<ShadowJar>("shadowJar") {
+        archiveFileName.set("typechecker.jar")
+        mergeServiceFiles()
+    }
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
 }
